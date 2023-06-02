f4d9eb63df65a972fba3daafc90f95305a3e1b82
Thomas_Thabethe <thomasthabethe3@gmail.com>
2023-06-02 12:55:29 +0200

    Initial commit

---
+++ src/main/java/com/eviro/assessment/grad001/ThomasThabethe/EnviroBankImageProcessor/EnviroBankImageProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.eviro.assessment.grad001.ThomasThabethe.EnviroBankImageProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnviroBankImageProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnviroBankImageProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eviro/assessment/grad001/ThomasThabethe/EnviroBankImageProcessor/EnviroBankImageProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eviro.assessment.grad001.ThomasThabethe.EnviroBankImageProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnviroBankImageProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
